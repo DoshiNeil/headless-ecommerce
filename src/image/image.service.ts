6b04517562f37e85a42a64fed220de8292f0ac73
Neil Doshi <mail.neildoshi@gmail.com>
2024-08-02 12:03:10 +0530

    WIP: Creating Image Service
    adding the minio client and getting the presignedURL

--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Client } from 'minio';
 
 @Injectable()
-export class ImageService {}
+export class ImageService {
+  private readonly minioClient: Client;
+  constructor(private configService: ConfigService) {
+    this.minioClient = new Client({
+      endPoint: this.configService.get<string>('MINIO_ENDPOINT'),
+      port: this.configService.get<number>('MINIO_PORT'),
+      useSSL: false,
+      accessKey: this.configService.get<string>('MINIO_ACCESSKEY'),
+      secretKey: this.configService.get<string>('MINIO_SECRETKEY'),
+    });
+  }
+}
